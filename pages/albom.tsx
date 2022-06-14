c386b3e3c4ab9beba30f9b9f85bd551fd03bee6e
AlexUAKH <sashaos1404@gmail.com>
2022-06-14 11:29:37 +0300

    added albom and tracks pages

---
+++ pages/albom.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { NextPage } from "next";
+import styles from "../styles/Albom.module.scss";
+import MainLayout from "../layouts/MainLayout";
+
+const Albom: NextPage = () => {
+  return (
+    <MainLayout>
+      <div>Albomm</div>
+    </MainLayout>
+  )
+}
+
+export default Albom;
