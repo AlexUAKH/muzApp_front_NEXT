95f24b842363347eecdd948bfbee1ad654d07aef
AlexUAKH <sashaos1404@gmail.com>
2022-06-27 11:39:19 +0300

    create useActions hook to binde all actions-creators

--- hooks/useActions.ts
+++ hooks/useActions.ts
@@ -2,7 +2,7 @@ import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import ActionCreators from "../store/action-creators";
 
-export const useActuons = () => {
+export const useActions = () => {
   const dispatch = useDispatch()
   return bindActionCreators(ActionCreators, dispatch)
 }
