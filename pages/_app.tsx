1c0302cdbfbea5cbaa9efebf3947474a5e0e12c0
AlexUAKH <sashaos1404@gmail.com>
2022-06-27 12:54:34 +0300

    added the main layout to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import React, {FC} from 'react';
 import {AppProps} from 'next/app';
 import {wrapper} from '../store';
 import "../styles/main.scss";
+import MainLayout from "../layouts/MainLayout";
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => <Component {...pageProps} />;
+const WrappedApp: FC<AppProps> = ({Component, pageProps}) => {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />;
+    </MainLayout>
+  )
+}
 
 export default wrapper.withRedux(WrappedApp);
 
