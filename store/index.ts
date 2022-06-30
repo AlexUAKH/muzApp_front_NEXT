611df08d123f7b3f23e9fcdf1f873317aed6893f
AlexUAKH <sashaos1404@gmail.com>
2022-06-30 09:36:23 +0300

    add thunk middleware

--- store/index.ts
+++ store/index.ts
@@ -1,7 +1,8 @@
 import {Context, createWrapper, MakeStore} from "next-redux-wrapper";
 import {reducer, RootState} from "./reducers";
-import {createStore, Store} from "redux";
+import {applyMiddleware, createStore, Store} from "redux";
+import thunk from "redux-thunk";
 
-const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer);
+const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer, applyMiddleware(thunk));
 
 export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
