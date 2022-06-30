5a4f1a5b7612fe99384794747ac859e663bbb8ba
AlexUAKH <sashaos1404@gmail.com>
2022-06-30 09:37:05 +0300

    create useInput hook

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -1,20 +1,33 @@
-// interface returnInput {
-//   error: string[];
-//   value: string;
-//   onChange:
-// }
+interface useInputResult {
+  error: boolean;
+  value: string;
+  onChange: Function;
+  helperText: string;
+}
 
-import {useState} from "react";
+interface validationObject {
+  require?: boolean;
+  minLength?: number;
+  maxLength?: number;
+}
 
-export default function useInput(validation) {
+interface useInputProps {
+  validation?: validationObject[];
+  initialValue?: string;
+}
+
+import React, {useState} from "react";
+
+export default function useInput({initialValue = '', validation}: useInputProps): useInputResult {
   const error: boolean = false;
-  const [value, setValue] = useState('');
-  const [touched, setTouched] = useState(false);
-  const [helperText, setHelperText] = useState('');
+  const [value, setValue] = useState<string>(initialValue);
+  const [touched, setTouched] = useState<boolean>(false);
+  const [helperText, setHelperText] = useState<string>('');
 
-  const onChange = (e) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
     setTouched(true)
+    console.log("change: ", e)
   }
   
   return {
