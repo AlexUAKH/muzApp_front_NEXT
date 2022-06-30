968acd4ef3be38fdf533aa8a08e20fc573176727
AlexUAKH <sashaos1404@gmail.com>
2022-06-30 09:32:22 +0300

    add backdrop to layout

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,26 +1,46 @@
-import React, {FC, ReactNode, useEffect} from "react";
+import React, {FC, ReactNode, useCallback, useEffect, useState} from "react";
 import Head from "next/head";
+import {useRouter} from "next/router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Container from "@mui/material/Container";
-import styles from "../styles/MainLayout.module.scss";
-import {useState} from "react";
 import Player from "../components/Player";
-import {Box} from "@mui/system";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import styles from "../styles/MainLayout.module.scss";
 
 interface Props {
   children: ReactNode;
 }
 
 const MainLayout: FC<Props> = ({children}) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const active = true;
 
-  useEffect(() => setLoading(false));
+  const onRouteChangeStart = useCallback(() => {
+    setLoading(true);
+  }, []);
 
-  if (loading) return <div>Loading...</div>;
+  const onRouteChangeDone = useCallback(() => {
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', onRouteChangeStart);
+    router.events.on('routeChangeComplete', onRouteChangeDone);
+    router.events.on('routeChangeError', onRouteChangeDone);
+
+    return () => {
+      router.events.off('routeChangeStart', onRouteChangeStart);
+      router.events.off('routeChangeComplete', onRouteChangeDone);
+      router.events.off('routeChangeError', onRouteChangeDone);
+    };
+  }, [onRouteChangeDone, onRouteChangeStart, router.events]);
 
   return (
+    <>
     <div className={styles.wraper}>
       <Head>
         <title>Music library</title>
@@ -28,6 +48,7 @@ const MainLayout: FC<Props> = ({children}) => {
           name="description"
           content="Music library with player and comments"
         />
+        <meta name="viewport" content="width=device-width, user-scalable=yes" />
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <Navbar/>
@@ -40,6 +61,17 @@ const MainLayout: FC<Props> = ({children}) => {
       <Player />
       <Footer/>
     </div>
+    <Backdrop
+      sx={{
+        color: '#fff',
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        backdropFilter: 'blur(8px)'
+      }}
+      open={loading}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+    </>
   );
 };
 
