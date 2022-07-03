6e4f301fbc8aca5d6b1a3bda83639a128db228fe
AlexUAKH <sashaos1404@gmail.com>
2022-07-03 16:47:41 +0300

    configure store to thunk

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,20 @@
 import {Context, createWrapper, MakeStore} from "next-redux-wrapper";
 import {reducer, RootState} from "./reducers";
-import {applyMiddleware, createStore, Store} from "redux";
-import thunk from "redux-thunk";
+import {AnyAction, applyMiddleware, createStore, Store } from "redux";
+import thunk, {ThunkDispatch} from "redux-thunk";
 
-const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer, applyMiddleware(thunk));
+const bindMiddleware = (middleware: any) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const {composeWithDevTools} = require('redux-devtools-extension');
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+  applyMiddleware(...middleware);
+}
+
+const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer, bindMiddleware([thunk]));
+
+
 
 export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
+
+export type NextThunkDispatch = ThunkDispatch<RootState, void, AnyAction>
