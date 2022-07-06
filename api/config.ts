28076979709f7e57fa413f7ecb47ccbc8145f945
AlexUAKH <sashaos1404@gmail.com>
2022-07-06 09:15:37 +0300

    url changed

--- api/config.ts
+++ api/config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export const config = {
-  API_URL: "https://localhost:4000",
+  API_URL: "http://localhost:4000",
   TIMEOUT: 10000
 }
 
